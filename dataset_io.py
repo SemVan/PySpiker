15cc5579ea722cbb239ee46b4d1ea545114d6d3c
Konstantin Zlobin <zlobinkd@mail.ru>
2020-02-16 18:46:04 +0300

    testing set added

--- dataset_io.py
+++ dataset_io.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import random
 
 DATASET_NAME = 'dataset.csv'
 
@@ -24,12 +25,25 @@ def read_dataset(file_name):
 def GRU_get_training_set(signals, sample_length):
     
     training_set = []
+    testing_set = []
     dataset_length = len(signals)
+    training_length = 0.8*dataset_length
     signal_length = len(signals[0][0])
-    n = signal_length % sample_length
+    n = signal_length // sample_length
+    j = -1
     for (contactless,contact) in signals:
-        for i in range(n):
-            training_set.append( (contactless[i*sample_length : (i+1)*sample_length], 
-                                  contact[i*sample_length : (i+1)*sample_length]) )
-            
-    return training_set
+        j += 1
+        if j < training_length:
+            for i in range(n):
+                training_set.append( (contactless[i*sample_length : (i+1)*sample_length], 
+                                      contact[i*sample_length : (i+1)*sample_length]) )
+        else:
+            for i in range(n):
+                testing_set.append( (contactless[i*sample_length : (i+1)*sample_length], 
+                                     contact[i*sample_length : (i+1)*sample_length]) )
+                
+    return random.shuffle(training_set), testing_set
+
+
+Signals = read_dataset(DATASET_NAME)
+Training_set, Testing_set = GRU_get_training_set(Signals, 150)
