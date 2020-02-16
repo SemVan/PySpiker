6cc29523a88a878245b126ffee7d3b3d5b162807
Konstantin Zlobin <zlobinkd@mail.ru>
2020-02-16 17:46:34 +0300

    training set getter

--- dataset_io.py
+++ dataset_io.py
@@ -4,16 +4,32 @@ import numpy as np
 DATASET_NAME = 'dataset.csv'
 
 def read_dataset(file_name):
-    contacts = []
-    contactlesses = []
+    
+    signals = []
+    contact = []
     with open(file_name, 'r') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',', quoting = csv.QUOTE_NONNUMERIC)
-        i = 0
-        for row in reader:
+        i = -1
+        for signal in reader:
+            contactless = contact
+            contact = signal
             i += 1
             if (i % 2) > 0:
-                contactlesses.append(row)
-            else:
-                contacts.append(row)    
+                signals.append((np.asarray(contactless), 
+                                np.asarray(contact)))   
 
-    return np.asarray(contacts), np.asarray(contactlesses)
+    return signals
+
+
+def GRU_get_training_set(signals, sample_length):
+    
+    training_set = []
+    dataset_length = len(signals)
+    signal_length = len(signals[0][0])
+    n = signal_length % sample_length
+    for (contactless,contact) in signals:
+        for i in range(n):
+            training_set.append( (contactless[i*sample_length : (i+1)*sample_length], 
+                                  contact[i*sample_length : (i+1)*sample_length]) )
+            
+    return training_set
